262add7aab83dfb972d8dfe2e016fc8b1e95b61e
Lakoffie <mafisajayaberkah@gmail.com>
2025-12-24 12:16:38 +0700

    Add files via upload

---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header.jsx"
+import Hero from "./components/Hero.jsx"
+import ProductList from "./components/ProductList.jsx"
+import WhatsAppCTA from "./components/WhatsAppCTA.jsx"
+import Footer from "./components/Footer.jsx"
+
+const App = () => (
+  <div>
+    <Header />
+    <Hero />
+    <ProductList />
+    <WhatsAppCTA />
+    <Footer />
+  </div>
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<App />)
