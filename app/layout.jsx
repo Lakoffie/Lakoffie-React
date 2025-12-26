c7ddabd4ad5b3c842d75183de5730b6b4398ab74
Lakoffie <mafisajayaberkah@gmail.com>
2025-12-26 12:54:49 +0700

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,1 +1,18 @@
+import "./globals.css"
+import Navbar from "../components/Navbar"
 
+export const metadata = {
+  title: "Lakoffie",
+  description: "Kopi lokal berkualitas"
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body className="pt-16">
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  )
+}
