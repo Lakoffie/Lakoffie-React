9597cb79249b913558d306765390c578869a4891
Lakoffie <mafisajayaberkah@gmail.com>
2025-12-26 12:48:27 +0700

    Delete src/components/Hero.jsx

--- src/components/Hero.jsx
+++
@@ -1,8 +0,0 @@
-const Hero = () => (
-  <section className="main-hero">
-    <h2>Kopi Bubuk yang Tidak Sekadar Pahit</h2>
-    <p>Nyaman diminum harian di rumah, rasa ala cafe</p>
-  </section>
-)
-
-export default Hero
