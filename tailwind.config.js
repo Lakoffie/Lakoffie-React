5521021bda2b43f46f241fc30821d8cfdc73bc0e
Lakoffie <mafisajayaberkah@gmail.com>
2025-12-26 12:53:06 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,16 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#3B2F2F",
+        accent: "#C08552"
+      }
+    }
+  },
+  plugins: []
+}
