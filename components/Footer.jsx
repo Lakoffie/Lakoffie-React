2d161d06bef50453e1a409987ce4378fdeb50bee
Lakoffie <mafisajayaberkah@gmail.com>
2025-12-26 13:15:40 +0700

    Create Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        © {new Date().getFullYear()} Lakoffie Kopi Murni.  
+        Semua hak dilindungi.
+      </p>
+    </footer>
+  )
+}
