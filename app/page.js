83030d10609a53b128c9b70e7e8d8f75b31c75e5
Lakoffie <mafisajayaberkah@gmail.com>
2025-12-29 01:27:41 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ import { useState } from "react";
 
 /* ========================= */
 
-import Hero from "@/components/landing/Hero.jsx";
+import Hero from "../components/landing/Hero.jsx";
 import ProductGrid from "../components/landing/ProductGrid.jsx";
 import Testimonial from "../components/landing/Testimonial.jsx";
 import About from "../components/landing/About.jsx";
