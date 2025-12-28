6d4dcef37c6f0d0254c3efaa265b6f5c85537db6
Lakoffie <mafisajayaberkah@gmail.com>
2025-12-29 00:51:14 +0700

    Update Footer.jsx

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -1,5 +1,11 @@
 export default function Footer() {
   return (
+    <Image
+  src="/logo-lakoffie.png"
+  alt="Lakoffie"
+  width={32}
+  height={32}
+/>
     <footer className="bg-[#4b2e1f] text-amber-200">
       <div className="container mx-auto px-4 py-4 text-center text-sm tracking-wide">
         © 2026 | <span className="font-semibold">LAKOFFIE</span> | KOPI BUBUK MURNI
