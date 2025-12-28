1f539336c4710e60222dedc9fabf304624c3da33
Lakoffie <mafisajayaberkah@gmail.com>
2025-12-29 01:36:11 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
-import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
-import StickyWhatsapp from "@/components/StickyWhatsapp";
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
+import StickyWhatsapp from "../components/StickyWhatsapp";
 
 export const metadata = {
   title: "Lakoffie | Kopi Bubuk dengan Karakter Rasa Berbeda",
