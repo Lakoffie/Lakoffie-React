7b098f92daa93b83f3e8a86c9002040bc59106c8
Lakoffie <mafisajayaberkah@gmail.com>
2025-12-28 23:28:50 +0700

    Update Slider.jsx

--- components/ui/Slider.jsx
+++ components/ui/Slider.jsx
@@ -1,1 +1,19 @@
+"use client";
+import { useEffect, useState } from "react";
 
+export default function Slider({ items }) {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setIndex((prev) => (prev + 1) % items.length);
+    }, 3000);
+    return () => clearInterval(timer);
+  }, [items.length]);
+
+  return (
+    <div className="mb-10">
+      {items[index]}
+    </div>
+  );
+}
