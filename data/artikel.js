6be05b511814131b9c76c67610a0e0d9743afef9
Lakoffie <mafisajayaberkah@gmail.com>
2025-12-28 23:27:38 +0700

    Update artikel.js

--- data/artikel.js
+++ data/artikel.js
@@ -1,1 +1,26 @@
+const artikel = [
+  {
+    id: 1,
+    judul: "Robusta Temanggung Aroma Gula Jawa",
+    slug: "robusta-temanggung-aroma-gula-jawa",
+    ringkas: "Robusta khas Temanggung dengan aroma gula jawa.",
+    konten: "Ini isi lengkap artikel...",
+    desa: "Kedu",
+    kecamatan: "Kedu",
+    tag: ["robusta", "temanggung"],
+    gambar: "/images/robusta.jpg",
+  },
+  {
+    id: 2,
+    judul: "Kopi Lokal Salatiga Rasa Unik",
+    slug: "kopi-lokal-salatiga",
+    ringkas: "Perpaduan pahit dan asam.",
+    konten: "Isi lengkap artikel kedua...",
+    desa: "Tingkir",
+    kecamatan: "Tingkir",
+    tag: ["lokal", "salatiga"],
+    gambar: "/images/salatiga.jpg",
+  },
+];
 
+export default artikel;
