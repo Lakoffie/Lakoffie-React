b49f17d3b6d7aa4db9a9470391292edc1f592732
Lakoffie <mafisajayaberkah@gmail.com>
2025-12-28 23:30:32 +0700

    Update page.js

--- app/artikel/[slug]/page.js
+++ app/artikel/[slug]/page.js
@@ -1,1 +1,22 @@
+import artikel from "@/data/artikel";
 
+export async function generateMetadata({ params }) {
+  const data = artikel.find(a => a.slug === params.slug);
+
+  return {
+    title: data?.judul,
+    description: data?.ringkas,
+  };
+}
+
+export default function DetailArtikel({ params }) {
+  const data = artikel.find(a => a.slug === params.slug);
+  if (!data) return <p>Artikel tidak ditemukan</p>;
+
+  return (
+    <article className="prose max-w-3xl mx-auto">
+      <h1>{data.judul}</h1>
+      <p>{data.konten}</p>
+    </article>
+  );
+}
