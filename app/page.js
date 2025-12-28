2df1adeba3bd8e8d268c3c4a8bca3913c9efce18
Lakoffie <mafisajayaberkah@gmail.com>
2025-12-29 03:03:44 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,21 +1,3 @@
-"use client";
-
-import AppShell from "../components/layout/AppShell.jsx";
-
-import Hero from "../components/landing/Hero.jsx";
-import ProductGrid from "../components/landing/ProductGrid.jsx";
-import Testimonial from "../components/landing/Testimonial.jsx";
-import About from "../components/landing/About.jsx";
-import FinalCTA from "../components/landing/FinalCTA.jsx";
-
 export default function Home() {
-  return (
-    <AppShell>
-  {/* <Hero /> */}
-  {/* <ProductGrid /> */}
-  {/* <Testimonial /> */}
-  {/* <About /> */}
-  {/* <FinalCTA /> */}
-</AppShell>
-  );
+  return <h1>TEST HOME</h1>;
 }
