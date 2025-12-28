fc41f75ea294bb8f97538fbe1524fb440228860b
Lakoffie <mafisajayaberkah@gmail.com>
2025-12-29 01:27:25 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -18,10 +18,10 @@ import { useState } from "react";
 /* ========================= */
 
 import Hero from "@/components/landing/Hero.jsx";
-import ProductGrid from "@/components/landing/ProductGrid.jsx";
-import Testimonial from "@/components/landing/Testimonial.jsx";
-import About from "@/components/landing/About.jsx";
-import FinalCTA from "@/components/landing/FinalCTA.jsx";
+import ProductGrid from "../components/landing/ProductGrid.jsx";
+import Testimonial from "../components/landing/Testimonial.jsx";
+import About from "../components/landing/About.jsx";
+import FinalCTA from "../components/landing/FinalCTA.jsx";
 
 export default function Home() {
   // const [filtered, setFiltered] = useState(artikel);
