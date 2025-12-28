4e0af21109b03c10e4b2f4acfde87d636f5d2a22
Lakoffie <mafisajayaberkah@gmail.com>
2025-12-29 00:05:14 +0700

    Create products.js

---
+++ data/products.js
@@ -0,0 +1,49 @@
+const products = [
+  {
+    id: 1,
+    name: "Lokal Salatiga",
+    desc: "Pahit dengan sensasi asam",
+    detail: "Pahit ringan dengan sentuhan asam segar khas kopi lokal.",
+    price: 13000,
+    oldPrice: 15000,
+    image: "/images/salatiga.jpg",
+  },
+  {
+    id: 2,
+    name: "Robusta Temanggung",
+    desc: "Pahit beraroma gula jawa",
+    detail: "Aroma manis alami berpadu pahit klasik.",
+    price: 13000,
+    oldPrice: 15000,
+    image: "/images/temanggung.jpg",
+  },
+  {
+    id: 3,
+    name: "Robusta Lampung",
+    desc: "Pahit nendang, body kental",
+    detail: "Bold dan mantap untuk penikmat kopi hitam.",
+    price: 13000,
+    oldPrice: 15000,
+    image: "/images/lampung.jpg",
+  },
+  {
+    id: 4,
+    name: "Arabika Salatiga",
+    desc: "Asam elegan",
+    detail: "Asam bersih dan seimbang ala café.",
+    price: 15000,
+    oldPrice: 18000,
+    image: "/images/arabika.jpg",
+  },
+  {
+    id: 5,
+    name: "Kopi Kantong Celup",
+    desc: "Cold brew praktis, tanpa ampas",
+    detail: "Tinggal seduh & dinginkan, cocok buat es kopi susu.",
+    price: 17000,
+    oldPrice: 20000,
+    image: "/images/celup.jpg",
+  },
+];
+
+export default products;
