fc576bd359727e205be1f7787f48f97423ebe205
Lakoffie <mafisajayaberkah@gmail.com>
2025-12-29 10:06:13 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,3 +1,19 @@
+"use client";
+
+import Hero from "../components/landing/Hero.jsx";
+import ProductGrid from "../components/landing/ProductGrid.jsx";
+import Testimonial from "../components/landing/Testimonial.jsx";
+import About from "../components/landing/About.jsx";
+import FinalCTA from "../components/landing/FinalCTA.jsx";
+
 export default function Home() {
-  return <h1>TEST HOME</h1>;
+  return (
+    <>
+      <Hero />
+      <ProductGrid />
+      <Testimonial />
+      <About />
+      <FinalCTA />
+    </>
+  );
 }
